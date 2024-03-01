83b1da78cf950ee1c952808065b39a93008ab5dd
Abhinav Utkarsh <abhina.utkarsh@gmail.com>
2024-03-01 01:55:16 +0100

    recent projects added

--- my_site/src/App.js
+++ my_site/src/App.js
@@ -12,21 +12,27 @@ function App() {
         <h1 className="name">ABHINAV</h1>
         <h1 className="name2"><br/>UTKARSH</h1>
         <div className="icon-container">
-          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/AbhinavUtkarsh" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} />
           </a>
-          <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/abhinavutkarsh" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-          <a href="mailto:your.email@example.com">
+          <a href="mailto:abhinav.utkarsh@tum.de">
             <FontAwesomeIcon icon={faEnvelope} />
           </a>
         </div>
-        <img src={profilePic} alt="Abhinav Utkarsh" className="profile-photo"/>
+        <div className="photo-wrapper">
+          <img src={profilePic} alt="Abhinav Utkarsh" className="profile-photo"/>
+        </div>
         <p className="intro-text">
         Currently, a master's candidate at the Technical University of Munich, majoring in Robotics, Cognition, and Intelligence, I am interested in 3D computer vision and deep learning, specifically in 3D shape completion. This forms the cornerstone of my current master's thesis work, hoping to contribute to the next generation of spatial computing. Moreover, I am interested in developing real-time models optimized explicitly for mobile silicon and deploying them to a larger audience. Beyond academics, I appreciate music from Coldplay and learning languages.
         </p>
       </header>
+       <section className="recent-projects">
+        <h2 className="section-title">Recent Projects</h2>
+        {/* Content for projects would go here */}
+      </section>
     </div>
   );
 }
